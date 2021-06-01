332c46f49b9b36ee5e41e7dccdb14ab1397a2ecc
Wojciech Snopek <Wojciech.Snopek@askom.pl>
2021-06-01 22:15:38 +0200

    Added error page, changed the backgrond resolution, added missing routes

--- HouseManagement/src/app/routing.module.ts
+++ HouseManagement/src/app/routing.module.ts
@@ -1,3 +1,4 @@
+import { TasksComponent } from './features/tasks/tasks.component';
 import { MainPageComponent } from './shared/main-page/main-page.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
@@ -9,10 +10,11 @@ import { LoginPageComponent } from './shared/login-page/login-page.component';
 const routes: Routes = [
   { path: 'payment', component: PaymentComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'main-page', component: MainPageComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'main', component: MainPageComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: '', redirectTo: '/main-page', pathMatch: 'full' }, // redirect to `first-component`
-  { path: '**', component: ErrorPageComponent },  // Wildcard route for a 404 page
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', component: ErrorPageComponent },
 ];
 
 @NgModule({
