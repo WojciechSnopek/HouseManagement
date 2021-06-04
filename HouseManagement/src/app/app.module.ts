a606014c3d477661851f06f08f4104cc41ffd610
Wojciech Snopek <Wojciech.Snopek@askom.pl>
2021-06-04 23:33:13 +0200

    basic routing setup, changed navigation components structure

--- HouseManagement/src/app/app.module.ts
+++ HouseManagement/src/app/app.module.ts
@@ -3,32 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '../app/routing.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppComponent } from './app.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 import { LoginPageComponent } from './shared/login-page/login-page.component';
-import { MainPageComponent } from './shared/main-page/main-page.component';
+import { HeaderComponent } from './shared/header/header.component';
 import { TasksComponent } from './features/tasks/tasks.component';
 import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
 import { PaymentComponent } from './features/payment/payment.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './shared/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ErrorPageComponent,
     LoginPageComponent,
-    MainPageComponent,
+    HeaderComponent,
     TasksComponent,
     ShoppingListComponent,
-    PaymentComponent
+    PaymentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- HouseManagement/src/app/routing.module.ts
+++ HouseManagement/src/app/routing.module.ts
@@ -1,19 +1,19 @@
 import { TasksComponent } from './features/tasks/tasks.component';
-import { MainPageComponent } from './shared/main-page/main-page.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 import { ShoppingListComponent } from './features/shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaymentComponent } from './features/payment/payment.component';
 import { LoginPageComponent } from './shared/login-page/login-page.component';
+import { HomeComponent } from './shared/home/home.component';
 
 const routes: Routes = [
   { path: 'payment', component: PaymentComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'tasks', component: TasksComponent },
-  { path: 'main', component: MainPageComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginPageComponent },
-  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: ErrorPageComponent },
 ];
 
